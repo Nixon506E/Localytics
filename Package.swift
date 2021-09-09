aa0996adf4b3e161ce4f876c76a7a2da853c867f
Keith Lamprecht <1894492+Nixon506E@users.noreply.github.com>
2021-09-09 12:48:28 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,10 +13,14 @@ let package = Package(
     ],
     dependencies: [],
     targets: [
+//         .binaryTarget(
+//             name: "Localytics",
+//             url: "https://downloads.localytics.com/SDKs/iOS/Localytics-iOS-6.2.1.zip",
+//             checksum: "c73bd3a8dae1a11717849d24a0df1f99ed6d9d1cc95df8ff73c6835073d53bf2"
+//         ),
         .binaryTarget(
             name: "Localytics",
-            url: "https://downloads.localytics.com/SDKs/iOS/Localytics-iOS-6.2.1.zip",
-            checksum: "c73bd3a8dae1a11717849d24a0df1f99ed6d9d1cc95df8ff73c6835073d53bf2"
-        ),
+            path: "Localytics.xcframework"
+        )
     ]
 )
